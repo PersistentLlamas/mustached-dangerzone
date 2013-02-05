76e2d06dd48f221908149e079e9220f2fbfffd2e
Jon Philo <philoj@xavier.edu>
2013-02-05 17:00:07 -0500

    Comments and methods for Department class - JP
    accessors and mutators

--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Department.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Department.java
@@ -1,6 +1,41 @@
 package com.example.xaviercontentmanagementsystem;
 
-public class Department {
-	private String Abbreviation = "";		//Abbreviation of Department i.e. 'CSCI'
-	private String Name = "";				//Full name of Department i.e. Computer Science
+public class Department 
+{
+	private String _Abbreviation = "";		//Abbreviation of Department i.e. 'CSCI'
+	private String _Name = "";				//Full name of Department i.e. Computer Science
+	
+	/*****************************_Abbreviation***********************************************/
+	// Accessor method for the _Abbreviation property of the Department Method.
+	// @returns String property _Abbreviation
+	public String GetAbreviation()
+	{
+		return _Abbreviation;
+	}
+	
+	// Sets the _Abbreviation property
+	// @param String of the desired _Abbreviation
+	public void SetAbbreviation(String abv)
+	{
+		_Abbreviation = abv;
+	}
+	
+	/*******************************************************************************************/
+	
+	/**********************************_NAME****************************************************/
+	// Accessor method for the _Name property of the Department Method.
+	// @returns String property _Name
+	public String GetName()
+	{
+		return _Name;
+	}
+	
+	// Sets the _Name property
+	// @param String of the desired _Name
+	public void SetName(String name)
+	{
+		_Name = name;
+	}
+	
+	/*******************************************************************************************/
 }
