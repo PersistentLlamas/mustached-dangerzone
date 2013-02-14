96efae6bfe90967a4b7e3678c4d81586c306e7b4
Jon Philo <philoj@xavier.edu>
2013-02-14 16:18:32 -0500

    Added all remaining accessors and mutators with the exception of the professor class

--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Course.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Course.java
@@ -2,9 +2,111 @@ package com.example.xaviercontentmanagementsystem;
 
 public class Course {
 	private String _Name = "";				//Name of the Course	
-	private Professor _Proff;				//Professor of the Course
+	private Professor _Prof;				//Professor of the Course
 	private Department _Dept;				//Department of the Course
 	private String _CourseNumber = "";		//Course Number i.e 193P
 	private String _RoomNumber ="";			//Room Number
 	private String _Building = "";			//Building Course is held
+	
+	/*****************************_NAME*********************************************************/
+	// Accessor method for the _Name property of the Course class.
+	// @returns String property _Name
+	public String GetName()
+	{
+		return _Name;
+	}
+	
+	// Sets the _Name property
+	// @param String of the desired _Name
+	public void SetName(String name)
+	{
+		this._Name = name;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*****************************_PROF*********************************************************/
+	// Accessor method for the _Prof property of the Course class.
+	// @returns Professor property _Prof
+	public Professor GetProfessor()
+	{
+		return _Prof;
+	}
+	
+	// Sets the _Prof property
+	// @param Professor of the desired _Prof
+	public void SetName(Professor prof)
+	{
+		this._Prof = prof;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*************************************_DEPT*************************************************/
+	// Accessor method for the _Dept property of the Course class.
+	// @returns Department property _Dept
+	public Department GetDepartment()
+	{
+		return _Dept;
+	}
+	
+	// Sets the _Dept property
+	// @param Department of the desired _Dept
+	public void SetDepartment(Department dept)
+	{
+		this._Dept = dept;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*****************************_COURSENUMBER*************************************************/
+	// Accessor method for the _CourseNumber property of the Course class.
+	// @returns String property _CourseNumber
+	public String GetCourseNumber()
+	{
+		return _CourseNumber;
+	}
+	
+	// Sets the _CourseNumber property
+	// @param String of the desired _CourseNumber
+	public void SetCourseNumber(String courseNumber)
+	{
+		this._CourseNumber = courseNumber;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*******************************_ROOMNUMBER*************************************************/
+	// Accessor method for the _RoomNumber property of the Course class.
+	// @returns String property _RoomNumber
+	public String GetRoomNumber()
+	{
+		return _RoomNumber;
+	}
+	
+	// Sets the _RoomNumber property
+	// @param String of the desired _RoomNumber
+	public void SetRoomNumber(String roomNumber)
+	{
+		this._RoomNumber = roomNumber;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*****************************_BUILDING***********************************************/
+	// Accessor method for the _Building property of the Course class.
+	// @returns String property _Building
+	public String GetBuilding()
+	{
+		return _Building;
+	}
+	
+	// Sets the _Building property
+	// @param String of the desired _Building
+	public void SetBuilding(String building)
+	{
+		this._Building = building;
+	}
+	
+	/*******************************************************************************************/
 }
--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Event.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Event.java
@@ -5,4 +5,38 @@ public class Event {
 	//private String  _Link = "";
 	private String _Description = "";
 	//private String _DueDate = "";
+	
+	/*****************************_TITLE******************************************************/
+	// Accessor method for the _Title property of the Event class.
+	// @returns String property _Title
+	public String GetTitle()
+	{
+		return _Title;
+	}
+	
+	// Sets the _Title property
+	// @param String of the desired _Title
+	public void SetTitle(String title)
+	{
+		this._Title = title;
+	}
+	
+	/*******************************************************************************************/
+	
+	/*****************************_DESCRIPTION**************************************************/
+	// Accessor method for the _Description property of the Event class.
+	// @returns String property _Description
+	public String GetDescription()
+	{
+		return _Description;
+	}
+	
+	// Sets the _Description property
+	// @param String of the desired _Description
+	public void SetDescription(String desc)
+	{
+		this._Description = desc;
+	}
+	
+	/*******************************************************************************************/
 }
