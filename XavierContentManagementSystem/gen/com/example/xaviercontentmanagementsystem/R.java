2826fa62e29f248e729e926b7d492d05a3088af0
Jon Philo <philoj@xavier.edu>
2013-03-11 13:58:19 -0400

    Added interface files as well as beginning of an activity. - JP

--- XavierContentManagementSystem/gen/com/example/xaviercontentmanagementsystem/R.java
+++ XavierContentManagementSystem/gen/com/example/xaviercontentmanagementsystem/R.java
@@ -8,24 +8,53 @@
 package com.example.xaviercontentmanagementsystem;
 
 public final class R {
+    public static final class array {
+        public static final int priorities=0x7f050000;
+    }
     public static final class attr {
+    }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int LinearLayout01=0x7f090001;
+        public static final int category=0x7f090000;
+        public static final int event_edit_button=0x7f090004;
+        public static final int event_edit_description=0x7f090003;
+        public static final int event_edit_summary=0x7f090002;
+        public static final int insert=0x7f090006;
+        public static final int menu_settings=0x7f090005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_event_overview=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int event_edit=0x7f030002;
+        public static final int event_list=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f080000;
+        public static final int listmenu=0x7f080001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int action_settings=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int event_edit_confirm=0x7f060008;
+        public static final int event_edit_description=0x7f060007;
+        public static final int event_edit_summary=0x7f060006;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_settings=0x7f060002;
+        public static final int no_events=0x7f060005;
+        public static final int title_activity_event_overview=0x7f060003;
     }
     public static final class style {
         /** 
@@ -49,10 +78,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
