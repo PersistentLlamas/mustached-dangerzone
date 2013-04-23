c8cc259482e5ed34c24a3911197cc458ea93ba51
Carl Mai <maic@xavier.edu>
2013-04-23 15:00:27 -0400

    Made it so you cannot input notes without either of the fields having content within them. CM

--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Notes.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/Notes.java
@@ -2,11 +2,14 @@ package com.example.xaviercontentmanagementsystem;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Notes extends Activity {
 
@@ -27,9 +30,19 @@ public class Notes extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(submitButton.getContext(), NotesManagement.class);
-				startActivityForResult(i, ACTIVITY_CREATE);	
-				//  TODO  
+				String title = notesTitle.getText().toString();
+				String content = noteContent.getText().toString();
+				if(title.equals("") || content == null){
+					Toast t =Toast.makeText(getApplicationContext(), "Please enter a title", 5000);
+				    t.show(); 
+				}else if(content.equals("")){
+					Toast t =Toast.makeText(getApplicationContext(), "Please input the notes you want to save", 5000);
+				    t.show(); 
+				} else {
+					Intent i = new Intent(submitButton.getContext(), NotesManagement.class);
+					startActivityForResult(i, ACTIVITY_CREATE);	
+					//  TODO  Add database implementation
+				}
 			}
 		});
 		
