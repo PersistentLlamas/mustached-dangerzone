216b19832da698432d05e3bc003dea6694c7fd99
Jon Philo <philoj@xavier.edu>
2013-04-23 00:29:22 -0400

    More updated R.java, and fixed a missing file error (temporarily) - JP

--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/SimpleNotification.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/SimpleNotification.java
@@ -18,14 +18,6 @@ public class SimpleNotification extends Activity {
         setContentView(R.layout.activity_simple_notification);
     
 	}
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) 
-    {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.activity_simple_notification, menu);
-            return true;
-        
-    }
     
     // Send the notification when a the TriggerNotification button is clicked
     public void triggerNotification(View view)
