d9165b5f7643b481d3d4d8b0140062d03d429fa5
Jon Philo <philoj@xavier.edu>
2013-04-23 15:09:58 -0400

    Moved 'Add Note' button into action bar - JP

--- XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/NotesManagement.java
+++ XavierContentManagementSystem/src/com/example/xaviercontentmanagementsystem/NotesManagement.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,19 +17,6 @@ public class NotesManagement extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notes_management);
-		
-		final Button addNoteButton = (Button) findViewById(R.id.add_note);
-		
-		addNoteButton.setOnClickListener(new View.OnClickListener()
-		{
-			
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(addNoteButton.getContext(), Notes.class);
-				startActivityForResult(i, ACTIVITY_CREATE);	
-				//  TODO  
-			}
-		});
 	}
 
 	@Override
@@ -36,6 +24,22 @@ public class NotesManagement extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.notes_management, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.insert_note:
+			createEvent();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private void createEvent()
+	{
+		Intent i = new Intent(this, Notes.class);
+		startActivityForResult(i, ACTIVITY_CREATE);	
 	}
 
 }
